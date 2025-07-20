85fdfc9027a0c9a4bb778cce95bca299c13fb4e9
Cocopops <88467012+ClydequiSlide@users.noreply.github.com>
2025-07-20 10:22:53 +0200

    Update rush04.c

--- rush00/ex00/rush04.c
+++ rush00/ex00/rush04.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
 void	ft_putchar(char c);
 
 void	rush(int x, int y)
